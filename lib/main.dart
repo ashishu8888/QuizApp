679bbe11f84209b85fcc0aeb0ee368cc0f7e259c
Ashish <au2242524@gmail.com>
2022-04-10 23:19:34 +0530

    certain changes in frontend

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/Question.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,16 +14,16 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final question = const [
-    'what\'s your favourite animal',
-    'what\'s is your favourite colour'
+    'what\'s your favourite animal ?',
+    'what\'s your favourite colour ?'
   ];
 
-  var questionIndex = 0;
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      if (questionIndex + 1 < question.length) {
-        questionIndex++;
+      if (_questionIndex + 1 < question.length) {
+        _questionIndex++;
       }
     });
   }
@@ -32,22 +33,35 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 64, 194, 144),
           title: const Text('Quiz'),
         ),
         body: Column(children: [
-          // ignore: unnecessary_string_interpolations
-          Text('${question[questionIndex]}'),
+          Question(question[_questionIndex]),
           RaisedButton(
             child: const Text('Answer 1'),
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
+            elevation: 12,
+            color: const Color.fromARGB(255, 107, 225, 111),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
           ),
           RaisedButton(
+            autofocus: true,
             child: const Text('Answer 2'),
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
+            elevation: 12,
+            color: const Color.fromARGB(255, 232, 104, 95),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
           ),
           RaisedButton(
             child: const Text('Answer 3'),
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
+            elevation: 12,
+            color: const Color.fromARGB(255, 87, 169, 237),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
           )
         ]),
       ),
